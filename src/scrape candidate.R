539479eba9d198ad93868d206c1aaef42692faf1
unknown <jonathan.mellon@nuffield.ox.ac.uk>
2015-03-17 09:52:30 -0400

    basic scrape script

---
+++ src/scrape candidate.R
@@ -0,0 +1,68 @@
+setwd("D:/candidate")
+
+library(RCurl)
+library(XML)
+
+scrapeCandidateWebsite <- function(candidate.row, homepage = TRUE) {
+	folder <- paste(candidate.row$name, candidate.row$id, sep = "_")
+	folder <- gsub(" ", "_", folder)
+	dir.create(folder)
+	if(homepage) {
+	url <- candidate.row$homepage_url
+	} else {
+		url <- candidate.row$party_ppc_page_url
+	}
+	if(url=="") {
+		return(NULL)
+	}
+	scrapeDepthOne <- function(url) {
+		cleanURL <- function(x) {
+			x <- gsub("http[s]*:*/*w*\\.*", "", x)
+			return(x)
+		}
+		doc <- htmlParse(url)
+		links <- xpathSApply(doc, "//a/@href")
+		links[grepl("^/", links)] <- paste0(url, gsub("^/", "", links[grepl("^/", links)]))
+		links <- unique(c(url, links[grepl(cleanURL(url), cleanURL(links))]))
+		links <- unique(gsub("www\\.", "", links))
+		links <- as.character(links)
+		return(links)
+	}
+	links <- scrapeDepthOne(url = url)
+	
+	downloadPage <- function(url, folder) {
+		if(!grepl("\\.[[:alpha:]]+$", url)) {
+			download.file(url, 
+										destfile = paste0(folder, "/",
+																			gsub("[/:\\.]", "_", url), ".html"))
+		} else {
+			file.type <- strsplit(url, "\\.")[[1]] 
+			file.type <- file.type[length(file.type)]
+			download.file(URLencode(url), 
+										destfile = paste0(folder, "/",
+																			gsub("[/:\\.]", "_", url), ".", file.type))	
+		}
+	}
+	lapply(links, downloadPage, folder = folder)
+	
+	return(length(links))
+}
+
+candidates <- read.csv("_source/candidates.csv",
+				 stringsAsFactors = FALSE)
+
+for(i in 1:nrow(candidates)) {
+	candidate.row <- candidates[i, ]
+	print(candidate.row$name)
+	try(scrapeCandidateWebsite(candidate.row = candidate.row)	)
+}
+
+
+
+
+candidate.row <- candidates[44, ]
+candidate.row <- candidates[candidates$id==2607, ]
+
+
+
+getwd()
