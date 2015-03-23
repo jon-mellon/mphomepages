0e80520f6fab271c78e34ce73d5deb99e6d8982d
unknown <jonathan.mellon@nuffield.ox.ac.uk>
2015-03-23 14:15:44 +0000

    added https support for Linux

--- src/scrape_candidate.R
+++ src/scrape_candidate.R
@@ -5,10 +5,19 @@ current.date <- substr(date(),5, 10)
 current.date <- gsub(" ", "_", current.date)
 dir.create(current.date)
 setwd(current.date)
-source("C:/Users/Jon/Documents/mphomepages/src/scrape_functions.R")
 
-candidates <- read.csv("https://yournextmp.com/media/candidates.csv",
-				 stringsAsFactors = FALSE)
+if(Sys.info()["sysname"]=="Linux") {
+	download.file("https://yournextmp.com/media/candidates.csv", 
+								destfile = "candidates.csv",
+								method="curl")
+	candidates <- read.csv("candidates.csv",
+												 stringsAsFactors = FALSE)	
+} else {
+	source("C:/Users/Jon/Documents/mphomepages/src/scrape_functions.R")	
+	candidates <- read.csv("https://yournextmp.com/media/candidates.csv",
+												 stringsAsFactors = FALSE)
+	
+}
 
 for(i in 1:nrow(candidates)) {
 	candidate.row <- candidates[i, ]
